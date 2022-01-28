d77fcdc7e7df3d17ff2703e99173e81350af8c51
Sergey Madaminov <sergey.madaminov@gmail.com>
2022-01-28 14:11:46 -0600

    now papers look somewhat better

--- myblog/src/pages/research/index.js
+++ myblog/src/pages/research/index.js
@@ -4,25 +4,40 @@ import Layout from '../../components/layout'
 
 const MyResearch = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
+  const papers = data.allMdx.nodes
 
-	return (
-    	<Layout location={location} title={siteTitle} navValue="2">
-        	{
-            	data.allMdx.nodes.map(node => (
-                	<article key={node.id}>
-                		<h3>
-                    		<Link to={`/research/${node.slug}`}>
-                        		{node.frontmatter.title}
-	                        </Link>
-    	                </h3>
-			    <p>{node.frontmatter.authors}<br/>
-			    In {node.frontmatter.venue}. {node.frontmatter.misc}</p>
-        	            <p>Year: {node.frontmatter.date}</p>
-            	    </article>
-            	))
-        	}
-    	</Layout>
-	)
+  return (
+    <Layout location={location} title={siteTitle} navValue="2">
+      <h3>Publications</h3>
+      <ol style={{ listStyle: `none` }}>
+        {papers.map(paper => {
+          const title = paper.frontmatter.title
+
+          return (
+            <li key={paper.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={paper.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>
+		    {paper.frontmatter.authors}<br/>
+		    {paper.frontmatter.venue}. {paper.frontmatter.misc} {paper.frontmatter.date}
+		  </small>
+                </header>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
 }
 
 export const query = graphql`
